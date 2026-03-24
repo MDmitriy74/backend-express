b3ff31599810362e4c347ced29d7a22d44d6c922
MDmitriy74 <dmostovov74@gmail.com>
2026-03-24 19:16:32 +0500

    make all

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,34 +1,63 @@
 const express = require('express');
 const router = express.Router();
 
-let users = [
-  { "id": 1, "name": "Dima" },
-  { "id": 2, "name": "Vlad" }
-];
+// let users = [
+//   { "id": 1, "name": "Dima" },
+//   { "id": 2, "name": "Vlad" }
+// ];
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('mydb.db');
+db.run(`CREATE TABLE IF NOT EXISTS users (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   name text)`);
+
+const insert = "INSERT INTO users (name) VALUES (?)";
 
 router.get('/', function(req, res, next) {
-  res.json({ items: users });
+  db.all("SELECT id, name FROM users",[], (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Ошибка сервера при получении пользователей");
+    } else {
+      res.send(rows);
+    }
+  });
 });
 
 router.get('/:id', function(req, res, next) {
   const userId = parseInt(req.params.id);
-  const user = users.find(u => u.id === userId);
-
-  if (user) {
-    res.json(user);
-  } else {
-    res.status(404).json({ message: "User not found" });
-  }
+  const sql = "SELECT id, name FROM users WHERE id = ?";
+  db.get(sql, [userId], (err, row) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Ошибка сервера при поиске пользователя" });
+    }
+    if (row) {
+      res.json(row);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  });
 });
 
 router.post('/', function(req, res, next) {
-  const newUser = req.body;
-  if (newUser && newUser.id) {
-    users.push(newUser);
-    res.status(201).json(newUser);
-  } else {
+  const name = req.body.name;
+  if (name === "")
     res.status(400).json({ message: "Invalid user data" });
-  }
+
+  const insert = "INSERT INTO users (name) VALUES (?)";
+
+  db.run(insert, [name], function(err) {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Ошибка сервера при сохранении пользователя");
+    } else {
+      res.status(201).json({
+        id: this.lastID,
+        name: name
+      });
+    }
+  });
 });
 
 module.exports = router;
