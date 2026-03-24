d4459ef274325381c37a3fd8859c60fa78e38cbf
dos34 <V-VPermyakov@yandex.ru>
2026-03-24 19:01:36 +0500

    7 done

--- express-app/app.js
+++ express-app/app.js
@@ -14,6 +14,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    if (req.query.auth === 'true')
+        next();
+    else {
+        return res.status(401).send('Not authorized');
+    }
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
+let users = [
+  { "id": 1, "name": "Dima" },
+  { "id": 2, "name": "Vlad" }
+];
+
 router.get('/', function(req, res, next) {
-  let user_arr = {
-    items : [{
-   "id": 1,
-   "name": "Dima"
-},
-{
-  "id": 2,
-   "name": "Vlad"
-}]
+  res.json({ items: users });
+});
+
+router.get('/:id', function(req, res, next) {
+  const userId = parseInt(req.params.id);
+  const user = users.find(u => u.id === userId);
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ message: "User not found" });
   }
-  res.send(user_arr);
+});
+
+router.post('/', function(req, res, next) {
+  const newUser = req.body;
+  if (newUser && newUser.id) {
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).json({ message: "Invalid user data" });
+  }
 });
 
 module.exports = router;
